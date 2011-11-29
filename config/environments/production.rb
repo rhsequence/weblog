e3fcd9586a8260e606cb10ceaf92e5f4d429656f
Richard Hernandez <rhsequence@gmail.com>
2011-11-28 22:36:02 -0600

    Heroku again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Weblog::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
+  # Full error reports are disabled and caching is turned o
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
